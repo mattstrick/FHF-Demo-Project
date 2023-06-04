48c7e1976a8a4a46f0b5227e5578db0187c61d50
Matt Strick <matt.strick@gmail.com>
2023-06-03 21:43:40 -0500

    Remove boilerplate code and replace with section skeletons

---
+++ demoapp/src/app/components/DeliverySection.tsx
@@ -0,0 +1,8 @@
+export default function DeliverySection() {
+    return (
+        <section className="border-2 rounded border-slate-300 border-solid p-8">
+          <h2>Delivery</h2>
+        </section>
+    );
+  }
+  
---
+++ demoapp/src/app/components/PaymentSection.tsx
@@ -0,0 +1,8 @@
+export default function PaymentSection() {
+    return (
+        <section className="border-2 rounded border-slate-300 border-solid p-8">
+          <h2>Payment</h2>
+        </section>
+    );
+  }
+  
